83af5e7842e9a84ff857082aab8eacee9b31eecc
Sarmad rafique <sarmadrafique040@gmail.com>
2023-04-01 00:15:02 +0500

    Completed Account creation and validation project using OOP principles

--- exercises/account encrytion.cpp
+++ exercises/account encrytion.cpp
@@ -4,50 +4,97 @@ bool valid_email(string email){
 	bool valid = false;
 	int length = email.length();
 	for(int i=0;i<=length;i++){
-		if(email[i]=="@"){
+		if(email[i]=='@'){
 			valid = true;
 			break;
 		}
 	}
+	return valid;
+}
+bool valid_password(string password){
+	bool valid = false;
+	int length = password.length();
+	for(int i=0;i<length;i++){
+		if(int(password[i])<'A' || int(password[i])>'z'){
+			valid = true;
+			break;
+		}
+	}
+	return valid;
+}
+string encrypt_password(string password){
+	int length = password.length();
+	char new_password[length];
+	for(int i=0;i<length;i++){
+		new_password[i] = int(password[i]) + 53;
+	}
+	for(int i=0;i<length;i++){
+		password[i] = new_password[i];
+	}
+	return password;
 }
 class Account{
 	protected:
 		string name,email;
-		static int id;
-		string password;
+		string password,enc_password;
+		int id;
+		static int u_id;
 	public:
-		Person(){
+		Account(){
 			name = "No name";
-			age = 0;
+			email = "name@example.com";
+			u_id+=1;
+			id = u_id;
+		}
+		void get_name(){ 
+			cout<<"Enter Your Name: "<<endl;
+			getline(cin,name);
+			if(name.length()<5){
+				cout<<"Name Must be at least of 5 characters: "<<endl;	
+				get_name();
+			}
+		}
+		void get_email(){
+			cout<<"Enter Your Email: "<<endl;
+			getline(cin,email);
+			if(valid_email(email)){
+			}
+			else{
+				cout<<"Email Must have @ in it: "<<endl;
+				get_email();
+			}
+		}
+		void get_password(){
+			cout<<"Enter Password: "<<endl;
+			getline(cin,password);
+			if(password.length()<8){
+				cout<<"Password Must have at least 8 characters: "<<endl;
+				get_password();
+			}
+			if(valid_password(password)){
+				enc_password = encrypt_password(password);
+		}else{
+			cout<<"Password must have some special characters: "<<endl;
+			get_password();	
+		}
+	}
+		void details(){
+			cout<<"\nAccount Details: "<<endl<<endl;
+			cout<<"ID: "<<id<<endl;
+			cout<<"Name: "<<name<<endl;
+			cout<<"Email: "<<email<<endl;
+			cout<<"Password: "<<enc_password<<endl;
 		}
 		void create_account(){
-			cout<<"Enter Your Name: "<<endl;
-			cin.ignore();
-			getline(cin,name);
-			cout<<"Enter Your Email: "<<endl;
-			cin.ignore();
-			getline(cin,email);
-			cout<<"Enter Your Password: "<<endl;
-			cin.ignore();
-			getline(cin,password);
-		}
-		void details(){
-			cout<<"Name: "<<name<<endl;
-			cout<<"Age: "<<age<<endl;
-		}
-			Person copy_object(Person person1){
-			Person person2;
-			person2.name = person1.name;
-			person2.age = person1.age;
-			
-			return person2;
+			get_name();
+			get_email();
+			get_password();
+			cout<<"\nAccount Created Successfully: \n";
 		}
 };
+int Account::u_id = 0;
 int main(){
-	Person person;
-	person.set_props("Sarmad",18);
+	Account person;
+	person.create_account();
 	person.details();
-	cout<<"Data after Object Copy. \n";
-	cout<<"Name: "<<person.copy_object(person).name<<endl;
-	cout<<"Age: "<<person.copy_object(person).age<<endl;
 }
