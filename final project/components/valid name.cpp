19f9967278573c5efa10a6ac294a5ef72759d7be
Muhammad Sarmad <sarmadrafique040@gmail.com>
2023-04-13 15:04:30 +0500

    validated the User Input Name:

--- final project/components/valid name.cpp
+++ final project/components/valid name.cpp
@@ -1,50 +1,63 @@
 #include<iostream>
 using namespace std;
-string valid_length(string name){
-	while(name.length()<6){
-		cout<<name.length()<<endl;
-		cout<<"Name Must be of 6 characters: "<<endl;
-		getline(cin,name);
-		if(name.length()>=6){
-			return name;
-		}
-		valid_length(name);
-	}
-	return name;
-}
-bool valid_values(string name){
-	for(int i=0;i<name.length();i++){
-		while(name[i]<'A' || name[i]>'z'){		
-			return false;
-		}	
+bool valid_length(string name){
+	if(name.length()<4){
+		return false;
 	}
 	return true;
 }
-string valid_name(string name){
-//	while(!valid_length(name)){
-//		cout<<"Name Must be of 6 characters: "<<endl;
-//		getline(cin,name);
-//	}	
-	name = valid_length(name);
-//	while(!valid_values(name)){
-//		cout<<"Name Must contain Alphabetical Characters: "<<endl;
-//		cout<<"Enter Name Again: "<<endl;
-//		getline(cin,name);
-//		valid_length(name);
-//	}
-	return name;
+bool valid_values(string name){
+	int length = name.length();
+	for(int i=0;i<length;i++){
+		if(name[i]<'A' || name[i]>'z'){
+			if(name[i]!=' ')
+			return false;
+		}
+	}
+	return true;
+}
+bool valid_email(string email){
+	int length = email.length();
+	bool valid = true;
+	for(int i=0;i<length;i++){
+			if(email[i]=='@'){
+			valid = true;
+		}
+		if(email[i]<'A' || email[i]>'z'){
+			valid = false;
+			return false;
+		}
+	}
+	return valid;
 }
 class Name{
 	protected:
-		string name;
+		string name,email;
 	public:
 		void get_name(){
 			cout<<"Enter Name: "<<endl;
 			getline(cin,name);
-			name = valid_name(name);
+			while(!valid_length(name) || !valid_values(name)){
+				cout<<"Something Went Wrong: Enter Name Again: "<<endl;
+				getline(cin,name);
+			}
+		}
+		void get_email(){
+			cout<<"Enter Email: "<<endl;
+			getline(cin,email);
+			while(!valid_email(email)){
+				cout<<"Please Provide @ in email.Enter Again: "<<endl;
+				getline(cin,email);
+			}
+		}
+		void show(){
+			cout<<"Name: "<<name<<endl;
+			cout<<"Email: "<<email<<endl;
 		}
 };
 int main(){
 	Name n;
 	n.get_name();
+	n.get_email();
+	n.show();
 }
