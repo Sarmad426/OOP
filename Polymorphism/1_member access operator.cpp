392b82148cf61cbdba91b94e42949b9286c34779
Muhammad Sarmad <sarmadrafique040@gmail.com>
2023-05-07 13:01:51 +0500

    Member Access Operator and this pointer in C++.

---
+++ Polymorphism/1_member access operator.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Person{
+	private:
+		string name;
+		int age;
+	public:
+		Person(string name,int age){
+			this->name = name;
+			this->age = age;
+		}
+		void details(){
+			cout<<"Name: "<<name<<endl;
+			cout<<"Age: "<<age<<endl;
+		}
+};
+int main(){
+	Person p1("Sarmad",18);
+	Person p2("Hammad",21);
+	Person p3("Naveed",23);
+	p1.details();
+	p2.details();
+	p3.details();
+}
+/*
+	C++ this Pointer
+		this is a keyword that refers to the current instance of the class.
+		It can be used to pass current object as a parameter to another method.
+		It can be used to refer current class instance variable.
+		It can be used to declare indexers.
+*/
