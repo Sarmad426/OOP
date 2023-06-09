5db46b54571770231659f49b3c866b69df2c7e8d
Sarmad Rafique <112152732+Sarmad426@users.noreply.github.com>
2023-06-09 21:34:06 +0500

    virtual function class

---
+++ Finals/16.4.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class A{
+	public:
+		virtual void show(){
+			cout<<"Parent Class A... "<<endl;
+		}
+};
+class B: public A{
+	public:
+		void show(){
+			cout<<"Child Class B..."<<endl;
+		}
+};
+class C:public A{
+	public:
+		void show(){
+			cout<<"Child Class C..."<<endl;
+		}
+};
+int main(){
+	A obj1;
+	B obj2;
+	C obj3;
+	A *ptr;
+	ptr = &obj1;
+	ptr->show();
+	ptr = &obj2;
+	ptr->show();
+	ptr = &obj3;
+	ptr->show();
+}
